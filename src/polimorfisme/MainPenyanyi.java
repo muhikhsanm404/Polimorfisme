125f504f8d46dc6d97d76dbefc435d04a166ef6b
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:13:16 +0700

    Create MainPenyanyi.java

---
+++ src/polimorfisme/MainPenyanyi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package polimorfisme;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class MainPenyanyi {
+    
+ public static void main(String[]args){ 
+     Penyanyi penyanyi1 = new Penyanyi();
+     Penyanyi penyanyi2 = new Penyanyi("Noah");
+ }  
+}
