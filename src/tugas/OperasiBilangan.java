d527995f2fc6fb1c1c053b4c1d72259faac1ebd3
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:15:34 +0700

    Create OperasiBilangan.java

---
+++ src/tugas/OperasiBilangan.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class OperasiBilangan {
+  protected double A, B;
+  
+
+  public void tampilkan() {
+      System.out.println("### APLIKASI PERHITUNGAN (*/+-) ###");
+  }
+
+  public void setA(double A) {
+    this.A = A;
+  }
+
+  public void setB(double B) {
+    this.B = B;
+  }
+
+  public double getA() {
+    return this.A;
+  }
+
+  public double getB() {
+    return this.B;
+    
+  }
+}
