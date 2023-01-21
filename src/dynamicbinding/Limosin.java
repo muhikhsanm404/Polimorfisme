3dd4fcc9b22d9b19b456fea91716e9e7455eee25
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:08:27 +0700

    Create Limosin.java

---
+++ src/dynamicbinding/Limosin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dynamicbinding;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Limosin extends Sapi {
+    
+    @Override
+    public String toString(){
+        return "dan salah satu jenisnya Limosin";
+    }
+}
