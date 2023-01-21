31353fa333650cf656e76684c573772e7a2133df
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:13:32 +0700

    Create MainIPS.java

---
+++ src/staticbinding/MainIPS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package staticbinding;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class MainIPS {
+    public static void main(String[]args){
+        IPS ips = new Sosiologi();
+        ips.showMapel();
+        
+        Sosiologi sosiologi = new Sosiologi();
+        sosiologi.showMapel();
+    }
+}
+ 
