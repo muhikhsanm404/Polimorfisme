66b89f3b94203d0d9807f113861222e6410c6923
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:12:51 +0700

    Create Smartphone.java

---
+++ src/upcasting/Smartphone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upcasting;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Smartphone implements Android{
+    @Override
+    public void versi(){
+        System.out.println("Android");    
+ }
+}
