46b1974cad1f7cc5c6d9c14b730be1299bbd0a71
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:08:11 +0700

    Create Mobil.java

---
+++ src/downcasting/Mobil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package downcasting;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Mobil {
+    public void show(){
+         System.out.println("Class Mobil");
+    }
+    void ngebut(){
+        System.out.println("Bisa ngebut dari class Mobil");
+    }
+}
