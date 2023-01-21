4eb40eda608d9b547e1a2e924475a69a16e09b4b
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:15:46 +0700

    Create Penjumlahan.java

---
+++ src/tugas/Penjumlahan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Penjumlahan extends OperasiBilangan {
+      @Override
+    public void tampilkan(){
+      double a = this.getA();
+      double b = this.getB();
+      double hasil = A + B;
+      
+      System.out.println(A + " + " + B + " = " + hasil);
+  }
+}
