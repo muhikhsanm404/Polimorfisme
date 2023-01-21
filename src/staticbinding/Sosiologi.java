ac9a86e229662c972d1c91f21fd9cb5c8eaac610
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:13:43 +0700

    Create Sosiologi.java

---
+++ src/staticbinding/Sosiologi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package staticbinding;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Sosiologi extends IPS{
+    
+    @Override
+    public void showMapel(){
+        System.out.println("Sosiologi turunan dari mapel IPS");
+    }
+}
