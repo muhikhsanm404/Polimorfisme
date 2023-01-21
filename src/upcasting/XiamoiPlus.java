c4c689fcbf0831f82a327ebe3c5f81839078716a
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:12:48 +0700

    Create XiamoiPlus.java

---
+++ src/upcasting/XiamoiPlus.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upcasting;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class XiamoiPlus extends Smartphone{
+    public void tablet(){
+        System.out.println("Menggunakan Snapdragon");
+    }
+}
