e8e63ec36144ae20a094cffbc8c1728a6fad16ba
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:12:38 +0700

    Create Mamalia.java

---
+++ src/dynamicbinding/Mamalia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dynamicbinding;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Mamalia {
+    @Override
+    public String toString(){
+        return "Contoh Mamalia adalah ";
+    }
+}
