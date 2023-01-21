541952373a33f27c8e8d338809bb39e8cb97047e
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:07:51 +0700

    Create Mobil.java

---
+++ src/classcasting/Mobil.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classcasting;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class Mobil {
+    public void show(){
+        System.out.println("Class Mobil");
+    }
+}
