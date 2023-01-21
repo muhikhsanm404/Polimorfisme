9747ad98ae17d42ef1dbc1a94a596cc9cb5d8af0
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:08:08 +0700

    Create MobilBalap.java

---
+++ src/downcasting/MobilBalap.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package downcasting;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class MobilBalap extends Mobil{
+    @Override
+    public void show(){
+        System.out.println("Class Mobil Balap");
+    }
+    public void ngebut(){
+         System.out.println("Bisa ngebut");
+    }
+}
