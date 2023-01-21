0662560e757e358b5fe9746d2ba8f8d27f848c9c
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-21 18:13:03 +0700

    Create GalaxyNote.java

---
+++ src/upcasting/GalaxyNote.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upcasting;
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public class GalaxyNote extends Smartphone{
+    public void note(){
+        System.out.println("Menggunakan Exynos");
+    }
+}
